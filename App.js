2388c21f2c82ae90d4d2587da4de1a32f061effa
IftikharZahid <RaiZahidN@gmail.com>
2023-03-03 11:49:04 -0800

    comments added

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+// lets start business area.
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+  // lets start business area.
+
+}
+//this is styling area.
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
